a3ee816829cecd9d9ed2c42e1c8eb57bbe66903c
AlvesDsX <alvesdesigner41@gmail.com>
2024-10-20 17:12:08 -0400

    MAIN ATUALIZADO
    Código principal refinado.
    Os ajustes no código estão identificados com as cores em vermelho (retirado) e em verde (substituído).
    Ainda deixei alguns comentários que explicam os ajustes.

--- Diagramação de POO/main.py
+++ Diagramação de POO/main.py
@@ -1,67 +1,106 @@
 from CLASSES import *
 
-# Lista para armazenar todos os usuários registrados.
+#Lista para armazenar todos os usuários registrados.
 usuarios = []
 
+#Função para personalizar linhas e textos
+def titulo(txt):
+    print("-"*30)
+    print(txt)
+    print("-"*30)   
+
+def tipoUser(tipotxt):
+    print(tipotxt)
+
 usuarios = []
-def registrar_usuario():
-    tipo = input("\n------------------------\nDigite o tipo de usuário\n------------------------\n\naluno\n\nprofessor_tecnico\n\nprofessor_materia\n\n------------------------\nDigite o tipo de usuário\n------------------------\n\nEscolha sua opção: ").strip().lower()
-    print("\n-----------------\nRegistrar usuário\n-----------------\n")
-    nome = input("Digite o nome: ").strip()
-    matricula = input("Digite a matrícula: ").strip()
-    senha = input("Digite a senha: ").strip()
+#Primeiro, coletar a info. dos usuários, pois elas serão usadas durante o restante do programa.
+def coletarInformacoesUsers():
+    nome = input("Digite seu nome: ").strip()
+    matricula = input("Digite sua matrícula: ").strip()
+    senha = input("Digite sua senha: ").strip()
+    return{"Nome": nome, "Matrícula": matricula, "Senha": senha}
 
+#Escolher o tipo de usuário do sistema.
+def escolherTipoUser():
+    opcoes = {
+        '1': "Aluno",
+        '2': "Professor Técnico",
+        '3': "Professor Matéria"
+    }
 
+    while True:
+        titulo("   Categorias de Usuários:")
+        for opcao, descricao in opcoes.items():
+            print(f"{opcao}. {descricao}")
+
+        escolha = input("\nPor favor, insira seu tipo de usuário: ").strip().lower()
+
+#Verificação de número ou nome
+        if escolha in opcoes: #caso seja números
+            return opcoes[escolha]
+        elif escolha.capitalize() in opcoes.values():
+            return escolha.capitalize()
+        
+        print("\n\033[3mTipo de usuário inválido, tente novamente.\033[0m\n")
+
+#Fazer o registro desse usuário com base na escolha realizada no "escolherTipoUser()".
+def registrarUser():
+    escolha = escolherTipoUser()
+    titulo("    Registro de Usuário:")
+    tipoUser(f"Opção \033[1m{escolha}\033[0m escolhido com sucesso!\n")
+    infoUsers = coletarInformacoesUsers()
+    matricula = infoUsers["Matrícula"]
+
+#Verificação da matrícula.
     if any(usuario.matricula == matricula for usuario in usuarios):
         print("\nErro: Matrícula já cadastrada.")
         return
 
-    if tipo == 'aluno':
-        turma = input("Digite o turno: ").strip()
-        usuario = Aluno(nome, matricula, senha, turma)
-    elif tipo == 'professor_tecnico':
-        modalidade = input("Digite a modalidade: ").strip()
+    if escolha == 'Aluno':
+        turma = input("Digite seu turno: ").strip()
+        usuario = Aluno(infoUsers["Nome"], matricula, infoUsers["Senha"], turma)
+    elif escolha == 'Professor Técnico':
+        modalidade = input("Digite sua modalidade: ").strip()
         aulas = input("Digite as aulas: ").strip()
-        usuario = ProfessorTecnico(nome, matricula, senha, modalidade, aulas)
-    elif tipo == 'professor_materia':
+        usuario = ProfessorTecnico(infoUsers["Nome"], matricula, infoUsers["Senha"], modalidade, aulas)
+    elif escolha == 'Professor Matéria':
         aulas = input("Digite as aulas: ").strip()
-        usuario = ProfessorMateria(nome, matricula, senha, aulas)
+        usuario = ProfessorMateria(infoUsers["Nome"], matricula, infoUsers["Senha"], aulas)
     else:
-        print("Tipo de usuário inválido.")
+        print("\n\033[3mTipo de usuário inválido.\033[0m")
         return
 
     usuarios.append(usuario)
-    print("\n-----------------\nRegistrar usuário\n-----------------")
-    print("\nUsuário registrado com sucesso.")
+    titulo("Registro de Usuário:")
+    print("\n\033[3mUsuário registrado com sucesso!\033[0m")
 
 def fazer_login():
-  print("\n-----------------\n   Fazer login   \n-----------------")
-  matricula = input("\nDigite a matrícula: ").strip()
-  senha = input("Digite a senha: ").strip()
+  titulo("       Realizar login:")
+  matricula = input("\nDigite sua matrícula: ").strip()
+  senha = input("Digite sua senha: ").strip()
   
 
   for usuario in usuarios:
       if usuario.matricula == matricula and usuario.verificar_senha(senha):
-          print("\nLogin bem-sucedido.\n")
+          print("\n\033[1m\033[3mLogin perfeitamente efetuado.\033[0m")
           return usuario
-  print("\nLogin falhou: matrícula ou senha incorretos.")
-  print("\n-----------------\n   Fazer login   \n-----------------")
+  print("\n\033[3mFalha no login: Senha ou matrícula incorretas.\033[0m")
   return None
 
 def menu():
   while True:
-      print("\n-------------------------\nSistema de Jogos Externos\n-------------------------\n")
-      print("1 - Registrar novo usuário")
-      print("2 - Fazer login")
-      print("3 - Sair")
+      titulo("  Sistema de Jogos Externos")
+      print("1. Registrar novo usuário")
+      print("2. Fazer login")
+      print("3. Sair do sistema")
       opcao = input("\nEscolha uma opção: ").strip()
 
       if opcao == '1':
-          registrar_usuario()
+          registrarUser()
       elif opcao == '2':
           usuario_logado = fazer_login()
           if usuario_logado:
-              print(f"Bem-vindo, {usuario_logado.nome}!")
+              print(f"\033[1m\033[3mBem-vindo, {usuario_logado.nome}!\033[0m")
       elif opcao == '3':
           print("\nSaindo do sistema...")
           break
