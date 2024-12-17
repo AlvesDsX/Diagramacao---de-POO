e83b5959f166ff3f6d14aafa96ecb73bbd8cdefb
Paulo Gabriel <167150572+AlvesDsX@users.noreply.github.com>
2024-12-16 23:38:52 -0400

    Como estava antes modalidade.py

--- Diagramação de POO/CÓDIGO FINALIZADO/modalidade.py
+++ Diagramação de POO/CÓDIGO FINALIZADO/modalidade.py
@@ -7,31 +7,13 @@ class Modalidade():
         self.time = List[Aluno] = []
 
     def adicionarAluno(self, aluno: Aluno):
-        try:
-            if not isinstance(aluno, Aluno):
-                raise TypeError("Somente objetos do tipo 'Aluno' podem ser adicionados ao time.")
-            
-            if aluno in self.time:
-                print(f"Aluno {aluno.nome} já está no time de {self.modalidade}.")
-
-            else:
-                self.time.append(aluno)
-                print(f'Aluno {aluno.nome} foi adicionado ao time de {self.modalidade}.')
-        except TypeError as e:
-                        print(f"Erro: {e}")
-        except Exception as e:
-            print(f"Ocorreu um erro inesperado: {e}")
+        if aluno not in self.time:
+            self.time.append(aluno)
+            print(f'Aluno {aluno.nome} foi adicionado ao time de {self.modalidade}.')
 
     def visualizarTime(self):
-        try:
-            if not self.time:
-                 print(f"Não há alunos no time de {self.modalidade}.")
-                 return []
-            
-            for aluno in self.time:
-                print(f"Aluno: {aluno.nome}, Matrícula: {aluno.matricula}")
-            return self.time
-        except Exception as e:
-            print(f"Ocorreu um erro ao visualizar o time: {e}")
+        for aluno in self.time:
+            print(f"Aluno: {aluno.nome}, Matrícula: {aluno.matricula}")
+        return self.time
     
 #MODALIDADE OK
